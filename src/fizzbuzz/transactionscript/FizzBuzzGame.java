f45362173218f6159462d2c995deb1ac266759ae
k_taniguchi <t.kiminobu@gmail.com>
2017-08-23 20:56:26 +0900

    整合性チェックを追加＋スライドの修正

--- src/fizzbuzz/transactionscript/FizzBuzzGame.java
+++ src/fizzbuzz/transactionscript/FizzBuzzGame.java
@@ -3,6 +3,9 @@ package fizzbuzz.transactionscript;
 public class FizzBuzzGame {
 
     public static void start(int start, int end) {
+        if (start <= 0 && end <= 0) throw new IllegalArgumentException("1以上の整数しか受け付けません。");
+        if (start > end) throw new IllegalArgumentException("末項が初項より大きいです。");
+
         for (int naturalNumber = start; naturalNumber <= end; naturalNumber++) {
             if (naturalNumber % 3 == 0 && naturalNumber % 5 == 0) {
                 System.out.print("FizzBuzz");
