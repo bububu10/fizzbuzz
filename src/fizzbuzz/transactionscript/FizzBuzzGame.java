a601e323a0a1acd888a17222316f104b994d5d1b
bububu10 <t.kiminobu@gmail.com>
2017-08-24 18:53:10 +0900

    コメントを追記

--- src/fizzbuzz/transactionscript/FizzBuzzGame.java
+++ src/fizzbuzz/transactionscript/FizzBuzzGame.java
@@ -3,9 +3,12 @@ package fizzbuzz.transactionscript;
 public class FizzBuzzGame {
 
     public static void start(int start, int end) {
+        // 引数チェック：両方とも自然数か？
         if (start <= 0 && end <= 0) throw new IllegalArgumentException("1以上の整数しか受け付けません。");
+        // 引数チェック：末項と初項の関係は正しいか？
         if (start > end) throw new IllegalArgumentException("末項が初項より大きいです。");
 
+        // startからendまで、naturalNumberを1ずつ増やして変換しながらコンソール出力する
         for (int naturalNumber = start; naturalNumber <= end; naturalNumber++) {
             if (naturalNumber % 3 == 0 && naturalNumber % 5 == 0) {
                 System.out.print("FizzBuzz");
@@ -16,6 +19,7 @@ public class FizzBuzzGame {
             } else {
                 System.out.print(naturalNumber);
             }
+            // 項と項の間はスペース区切り、最後の項には次の項がないのでスペースを出力しない
             if (naturalNumber != end) System.out.print(" ");
         }
     }
