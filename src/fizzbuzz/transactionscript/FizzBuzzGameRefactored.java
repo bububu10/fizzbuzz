9a15777c887620086f493c1dab80c52e5331ded7
bububu10 <t.kiminobu@gmail.com>
2017-08-27 13:03:33 +0900

    コメントを追記

--- src/fizzbuzz/transactionscript/FizzBuzzGameRefactored.java
+++ src/fizzbuzz/transactionscript/FizzBuzzGameRefactored.java
@@ -11,6 +11,7 @@ public class FizzBuzzGameRefactored {
     }
 
     private static List<String> toFizzBuzzes(int start, int end) {
+        // startからendまでの連続した数をfizzbuzzのルールに基いて変換した文字列のリストを返す
         return IntStream
                 .rangeClosed(start, end)
                 .mapToObj(FizzBuzzGameRefactored::toFizzBuzz)
@@ -18,6 +19,7 @@ public class FizzBuzzGameRefactored {
     }
 
     private static String toFizzBuzz(int number) {
+        // 1つの数をfizzbuzzのルールに基いて変換して文字列として返す
         if (number % 3 == 0 && number % 5 == 0) return "FizzBuzz";
         if (number % 3 == 0) return "Fizz";
         if (number % 5 == 0) return "Buzz";
@@ -25,10 +27,12 @@ public class FizzBuzzGameRefactored {
     }
 
     private static String join(List<String> fizzBuzzTextList) {
+        // リストをスペース区切りで連結して１つの文字列にする
         return String.join(" ", fizzBuzzTextList);
     }
 
     private static void showConsole(String outputText) {
+        // コンソールへ文字列を出力する
         System.out.print(outputText);
     }
 }
